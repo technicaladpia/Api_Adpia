31d8c75d72858cba414992debe343186eb2234a1
technicaladpia <50009828+technicaladpia@users.noreply.github.com>
2019-07-01 15:30:25 +0700

    Create new file get all coupon/voucher

---
+++ ApiGetDiscountAll.php
@@ -0,0 +1,10 @@
+<?php 
+	// add api getCode to curl. 
+	$curl = curl_init('http://event.adpia.vn/apigetdiscount/getDiscountAll');
+	// call api getCode
+	curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+	// excute curl to get data
+	$result = curl_exec($curl);
+	curl_close($curl);
+	//respon data
+	var_dump($result);
