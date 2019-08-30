943058237ff0a24f1efcae6854603cd16861f770
technicaladpia <50009828+technicaladpia@users.noreply.github.com>
2019-08-30 14:02:39 +0700

    Demo new api get product

---
+++ ApiProductList.php
@@ -0,0 +1,20 @@
+    
+<?php 
+  $token = base64_encode('account_id:password'); 
+	//Let add infor your account
+	//add field to fillter exactly: status value(100:pending, 200:merchant confirm, 210:complete and payment, 300:cancel from merchant, 310:cancelled(Admin approved)), order_code, limit (limit field data respon), page (page pagination)
+	$curl = curl_init('http://event.adpia.vn/apiv2/productList');
+	// call api getConversions
+	curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "POST");
+	// method api
+	curl_setopt($curl, CURLOPT_POSTFIELDS, $dataPost);
+	// post api with data cause from input form
+	curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+	// sent token to encoding base64 contain infor your account 
+	curl_setopt($curl, CURLOPT_HTTPHEADER, array(
+		'authorization:Basic '.$token)
+	);
+	$result = curl_exec($curl);
+	curl_close($curl);
+	//respon data 
+	var_dump($result);
