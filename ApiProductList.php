fb500cfcd42d9cf71b4dcca6ad7eab7d78face0f
technicaladpia <50009828+technicaladpia@users.noreply.github.com>
2021-10-08 10:49:20 +0700

    Update ApiProductList.php

--- ApiProductList.php
+++ ApiProductList.php
@@ -3,7 +3,7 @@
   $token = base64_encode('account_id:password'); 
 	//Let add infor your account
 	//add field to fillter exactly: status value(100:pending, 200:merchant confirm, 210:complete and payment, 300:cancel from merchant, 310:cancelled(Admin approved)), order_code, limit (limit field data respon), page (page pagination)
-	$curl = curl_init('http://event.adpia.vn/apiv2/productList');
+	$curl = curl_init('https://event.adpia.vn/apiv2/productList');
 	// call api getConversions
 	curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "POST");
 	// method api
