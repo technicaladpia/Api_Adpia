feae0302d74d53dee7202d78188a4e3fb131a40e
technicaladpia <50009828+technicaladpia@users.noreply.github.com>
2021-10-08 10:49:06 +0700

    Update ApiGetMerchantConversions.php

--- ApiGetMerchantConversions.php
+++ ApiGetMerchantConversions.php
@@ -3,7 +3,7 @@
 	//Let add infor your account
 	$dataPost = array("sdate" => "20190530", "edate" => "20190530");
 	//add field to fillter exactly: status value(100:pending, 200:merchant confirm, 210:complete and payment, 300:cancel from merchant, 310:cancelled(Admin approved)), order_code, limit (limit field data respon), page (page pagination)
-	$curl = curl_init('http://event.adpia.vn/apiv2/getMerchantConversions');
+	$curl = curl_init('https://event.adpia.vn/apiv2/getMerchantConversions');
 	// call api getConversions
 	curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "POST");
 	// method api
