38449904eba46ef529a2d221b1489160b0bfd188
technicaladpia <50009828+technicaladpia@users.noreply.github.com>
2021-10-08 10:48:27 +0700

    Update ApiGetDiscountAll.php

--- ApiGetDiscountAll.php
+++ ApiGetDiscountAll.php
@@ -1,6 +1,6 @@
 <?php 
 	// add api getCode to curl. 
-	$curl = curl_init('http://event.adpia.vn/apigetdiscount/getDiscountAll');
+	$curl = curl_init('https://event.adpia.vn/apigetdiscount/getDiscountAll');
 	// call api getCode
 	curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 	// excute curl to get data
